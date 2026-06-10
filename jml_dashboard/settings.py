b50050922ca51a6666d4a24103ee271e3e35a4b0
Gennbu <jorgealbertovargas64@gmail.com>
2026-06-10 17:34:00 -0500

    Restore SQLite configuration to fix deploy

--- jml_dashboard/settings.py
+++ jml_dashboard/settings.py
@@ -71,23 +71,12 @@ WSGI_APPLICATION = 'jml_dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# Si hay una DATABASE_URL, usa PostgreSQL (Render), sino usa SQLite (local)
-import dj_database_url
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# Si la variable de entorno DATABASE_URL existe (Render), sobreescribe la BD
-if os.getenv('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
 
 
 # Password validation
